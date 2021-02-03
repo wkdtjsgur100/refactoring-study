6f156b7273de5e0983ef1caec5522f8553acd3f9
jang <wkdtjsgur100@naver.com>
2021-02-04 00:42:48 +0900

    Add play field to performance

--- refactoring.py
+++ refactoring.py
@@ -1,35 +1,29 @@
 import math
 
 def render_plain_text(data, plays):
-    def play_for(aPerformace):
-        # Remove temporary local variable. because temporary variables create a lot of locally scoped names that complicate extractions.
-        # this change is unlikely to significantly affect performance
-        # and even if it were, it is much easier to improve performance of a well-factored code base.
-        return plays[aPerformace["playID"]]
-
     def amount_for(aPerformance):
         # it's calculating the charge for one performance
         # rename some of the variables to make them clearer
         # Any fool can write code that a computer can under stance. Good programmers write code that humans can understand
 
-        if play_for(aPerformance)["type"] == "tragedy":
+        if aPerformance["play"]["type"] == "tragedy":
             result = 40000
             if aPerformance["audience"] > 30:
                 result += 1000 * (aPerformance["audience"] - 30)
-        elif play_for(aPerformance)["type"] == "comedy":
+        elif aPerformance["play"]["type"] == "comedy":
             result = 30000
             if aPerformance["audience"] > 20:
                 result += 10000 + 500 * (aPerformance["audience"] - 20)
             result += 300 * aPerformance["audience"]
         else:
-            raise ValueError("unknown type:" + play_for(aPerformance))
+            raise ValueError("unknown type:" + aPerformance["play"]["type"])
 
         return result
 
     def volume_credit_for(perf):
         result = max(perf["audience"] - 30, 0)
         # add extra credit for every ten comedy attendees
-        if "comedy" == play_for(perf)["type"]:
+        if "comedy" == perf["play"]["type"]:
             result += math.ceil(perf["audience"])
 
         return result
@@ -47,19 +41,27 @@ def render_plain_text(data, plays):
     result = f"Statement for {data['customer']}\n"
     # print line for this order
     for perf in data["performances"]:
-        result += f"    {play_for(perf)['name']}: {amount_for(perf) / 100} ({perf['audience']} seats)\n"
+        result += f"    {perf['play']['name']}: {amount_for(perf) / 100} ({perf['audience']} seats)\n"
 
     result += f"Amount owed is {total_amount() / 100}\n"
     result += f"You earned {total_volume_credits()} credits\n"
     return result
 
 
-def enrich_performance(aPerformance):
-    result = dict(aPerformance)
-    return result
 
 
 def statement(invoice, plays):
+    def play_for(aPerformace):
+        # Remove temporary local variable. because temporary variables create a lot of locally scoped names that complicate extractions.
+        # this change is unlikely to significantly affect performance
+        # and even if it were, it is much easier to improve performance of a well-factored code base.
+        return plays[aPerformace["playID"]]
+
+    def enrich_performance(aPerformance):
+        result = dict(aPerformance)
+        result["play"] = play_for(aPerformance)
+        return result
+
     # split phase
     # one is calculating the data required for the statement
     # the other is rendering it into text or HTML
