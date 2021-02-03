e4de84e72edb0ac62079bcfb485224670c979c51
jang <wkdtjsgur100@naver.com>
2021-02-03 17:42:46 +0900

    Remove local variables

--- refactoring.py
+++ refactoring.py
@@ -1,42 +1,45 @@
 import math
 
-def amount_for(aPerformance, play):
-    # it's calculating the charge for one performance
-    # rename some of the variables to make them clearer
-    # Any fool can write code that a computer can under stance. Good programmers write code that humans can understand
-
-    if play["type"] == "tragedy":
-        result = 40000
-        if aPerformance["audience"] > 30:
-            result += 1000 * (aPerformance["audience"] - 30)
-    elif play["type"] == "comedy":
-        result = 30000
-        if aPerformance["audience"] > 20:
-            result += 10000 + 500 * (aPerformance["audience"] - 20)
-        result += 300 * aPerformance["audience"]
-    else:
-        raise ValueError("unknown type:" + play["type"])
-
-    return result
-
-
 def statement(invoice, plays):
     total_amount = 0
     volume_credits = 0
 
+    def play_for(aPerformace):
+        # Remove temporary local variable. because temporary variables create a lot of locally scoped names that complicate extractions.
+        # this change is unlikely to significantly affect performance
+        # and even if it were, it is much easier to improve performance of a well-factored code base.
+        return plays[aPerformace["playID"]]
+
+    def amount_for(aPerformance):
+        # it's calculating the charge for one performance
+        # rename some of the variables to make them clearer
+        # Any fool can write code that a computer can under stance. Good programmers write code that humans can understand
+
+        if play_for(aPerformance)["type"] == "tragedy":
+            result = 40000
+            if aPerformance["audience"] > 30:
+                result += 1000 * (aPerformance["audience"] - 30)
+        elif play_for(aPerformance)["type"] == "comedy":
+            result = 30000
+            if aPerformance["audience"] > 20:
+                result += 10000 + 500 * (aPerformance["audience"] - 20)
+            result += 300 * aPerformance["audience"]
+        else:
+            raise ValueError("unknown type:" + play_for(aPerformance))
+
+        return result
+
     result = f"Statement for {invoice['customer']}\n"
     for perf in invoice["performances"]:
-        play = plays[perf["playID"]]
-        this_amount = amount_for(perf, play)
         # add Volume credits
         volume_credits += max(perf["audience"] - 30, 0)
         # add extra credit for every ten comedy attendees
-        if "comedy" == play["type"]:
+        if "comedy" == play_for(perf)["type"]:
             volume_credits += math.ceil(perf["audience"])
 
         # print line for this order
-        result += f"    {play['name']}: {this_amount / 100} ({perf['audience']} seats)\n"
-        total_amount += this_amount
+        result += f"    {play_for(perf)['name']}: {amount_for(perf) / 100} ({perf['audience']} seats)\n"
+        total_amount += amount_for(perf)
 
     result += f"Amount owed is {total_amount / 100}\n"
     result += f"You earned {volume_credits} credits\n"
